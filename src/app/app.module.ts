1bd82b8e741b0f218c7ba53c2615f01b4ff4c0d6
Girish Sawant <girishsawant999.gs@gmail.com>
2020-04-26 01:38:55 +0530

    [Add] Bootstrap Template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { SidebarComponent } from './Components/sidebar/sidebar.component';
+import { HomeComponent } from './Pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
